a4a8dedbccdb899c36c12ec184b1aaaee30ea526
BAHAMLAOUHI <bahaeddine.mlaouhi@ensi-uma.tn>
2023-06-19 23:43:56 +0200

    schema

--- back/models/utilisateur.js
+++ back/models/utilisateur.js
@@ -1,15 +1,25 @@
 const mongoose = require('mongoose');
 
 const participantSchema = mongoose.Schema({
-  nom: { type: String, required: true },
-  prenom: { type: String, required: true },
-  imageUrl: { type: String, required: true },
-  userId: { type: String, default: () => {
-    // Génération de votre identifiant personnalisé
-    return generateCustomId();
-  } },
-  age: { type: Number, required: true },
-  numtel :{ type: Number, required: true },
-});
+  nom: {
+    type: String,
+    required: true 
+    
+  },
+  email :{
+    type: String,
+    required: true 
+  },
+  tel:{
+    type: String,
+    required: true 
+  },
+  profession:{
+    type:String,
+    required: true 
+  }
+}, { timestamps: true });
 
 module.exports = mongoose.model('participant', participantSchema);
+
+
