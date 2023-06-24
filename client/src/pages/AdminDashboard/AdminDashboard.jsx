778716fed31771fb74b87a3256ccd1f993449697
BAHAMLAOUHI <bahaeddine.mlaouhi@ensi-uma.tn>
2023-06-25 01:01:34 +0200

    final admin dashboard

--- client/src/pages/AdminDashboard/AdminDashboard.jsx
+++ client/src/pages/AdminDashboard/AdminDashboard.jsx
@@ -1,9 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import './AdminDashboard.css';
 
 function AdminDashboard() {
   const [participants, setParticipants] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [newParticipant, setNewParticipant] = useState({
+    nom: '',
+    email: '',
+    tel: '',
+    profession: ''
+  });
 
   useEffect(() => {
     const fetchParticipants = async () => {
@@ -22,29 +29,125 @@ function AdminDashboard() {
     fetchParticipants();
   }, []);
 
+  const handleDelete = async (id) => {
+    try {
+      await axios.delete(`http://localhost:3000/participants/${id}`);
+      setParticipants((prevParticipants) =>
+        prevParticipants.filter((participant) => participant._id !== id)
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleInputChange = (e) => {
+    setNewParticipant({
+      ...newParticipant,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post("http://localhost:3000/participants/", newParticipant);
+      const createdParticipant = response.data;
+      setParticipants([...participants, createdParticipant]);
+      setNewParticipant({
+        nom: '',
+        email: '',
+        tel: '',
+        profession: ''
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  
+
   return (
-    <div>
+    <div className="admin-dashboard">
+      <p className="participant-count">Total Participants: {participants.length}</p>
       {isLoading ? (
         <p>Loading participants...</p>
       ) : (
-        <ul>
-          {participants.map((participant) => (
-            <li key={participant._id}>
-              <hr></hr>
-              <p>Name: {participant.nom}</p>
-              <p>Email: {participant.email}</p>
-              <p>Phone: {participant.tel}</p>
-              <p>Profession: {participant.profession}</p>
-              <hr></hr>
-              <br />
-            </li>
-           
-          ))}
-        </ul>
+        <table className="participants-table">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Phone</th>
+              <th>Profession</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {participants.map((participant) => (
+              <tr key={participant._id}>
+                <td>{participant.nom}</td>
+                <td>{participant.email}</td>
+                <td>{participant.tel}</td>
+                <td>{participant.profession}</td>
+                <td>
+                  <button
+                    className="delete-button"
+                    onClick={() => handleDelete(participant._id)}
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       )}
+      
+      <form className="add-participant-form" method='post' onSubmit={handleSubmit}>
+        <h3>Add Participant</h3>
+        <div className="form-group">
+          <label>Name:</label>
+          <input
+            type="text"
+            name="nom"
+            value={newParticipant.nom}
+            onChange={handleInputChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label>Email:</label>
+          <input
+            type="email"
+            name="email"
+            value={newParticipant.email}
+            onChange={handleInputChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label>Phone:</label>
+          <input
+            type="tel"
+            name="tel"
+            value={newParticipant.tel}
+            onChange={handleInputChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label>Profession:</label>
+          <input
+            type="text"
+            name="profession"
+            value={newParticipant.profession}
+            onChange={handleInputChange}
+            required
+          />
+        </div>
+        <button type="submit">Add</button>
+      </form>
     </div>
   );
 }
-
 
 export default AdminDashboard;
